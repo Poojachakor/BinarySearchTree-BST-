9307da1f0223978dc8d7d3f8fd09e08518b16ba6
Poojachakor <chakorpooja07@gmail.com>
2023-03-26 17:39:12 +0530

    Add: BinarySearchTreeProject

---
+++ BinarySearchTreeProject/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BinarySearchTreeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BST binarySearchTree = new BST();
+            binarySearchTree.Insert(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
